dc63c1023d186632a6eb6de106f34d5add16e067
reha.burak.acar <reha.burak.acar@armagroupholding.com>
2022-06-09 17:52:11 +0300

    last changes

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from turtle import color
+
+from numpy import integer
 from transformers import pipeline
 import os
 import tkinter as tk
 import language_tool_python 
+from tkinter import *
 from tkinter import filedialog
 # from pdf import PdfFileReader
 import textract
@@ -11,6 +15,44 @@ import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 translator = Translator()
+
+root = Tk()
+root.geometry('+%d+%d'%(350,10)) #place GUI at x=350, y=10
+
+header = Frame(root, width=1000, height=450, bg="#52adc8")
+header.grid(columnspan=3, rowspan=2, row=0)
+#main content area - text and image extraction
+main_content = Frame(root, width=1000, height=450, bg="#52adc8")
+main_content.grid(columnspan=3, rowspan=2, row=4)
+
+frame = tk.Frame(root, bg="#52adc8")
+frame.place(relwidth=0.8, relheight=0.3, relx = 0.1, rely = 0.1)
+label = tk.Label(frame, text="All content of document will be shown here.", wraplength=1000,  fg="white", bg= "#52adc8")
+label.pack()
+
+frame2 = tk.Frame(root, bg="#52adc8")
+frame2.place(relwidth=0.8, relheight=0.3, relx = 0.1, rely = 0.65)
+label = tk.Label(frame2, text="Summarized text will be shown here.", wraplength=1000, fg="white", bg= "#52adc8" )
+label.pack()
+
+#BEGIN MENUES AND MENU WIDGETS
+#1.image menu on row 2
+# img_menu = Frame(root, width=800, height=60)
+# img_menu.grid(columnspan=3, rowspan=1, row=2)
+
+#what_img = Label(root, textvariable=what_text, font=("shanti", 10))
+
+#what_img.grid(row=2, column=1)
+#arrow buttons
+#2.save image menu on row 3
+save_img_menu = Frame(root, width=1000, height=60, bg="#387ca3")
+save_img_menu.grid(columnspan=3, rowspan=1, row=3)
+#create action buttons
+# summarizeTextFromPDF = tk.Button(root, text="Summarize Text From DOCX", padx=10, pady=5, fg="white", bg="#000000", command=summarizeFromPDF)
+# summarizeTextFromPDF.pack()
+
+# summarizeTextFromAudio = tk.Button(root, text="Summarize Text From Audio File", padx=10, pady=5, fg="white", bg="#000000", command=summarizeFromAudio)
+# summarizeTextFromAudio.pack()
 
 
 def makeMeaningful(my_text):
@@ -47,7 +89,7 @@ def makeMeaningful(my_text):
     
     # printing the text
 
-    print(my_NewText)
+    #print(my_NewText)
     return my_NewText 
 
 # create a speech recognition object
@@ -109,16 +151,12 @@ summarizer = pipeline("summarization")
 ## summarizer = pipeline("summarization", model="t5-base", tokenizer="t5-ba
 apps = []
 
-root = tk.Tk()
+#root = tk.Tk()
 
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
-canvas.pack()
+#canvas = tk.Canvas(root, height=700, width=700, bg="#000000")
+#canvas.pack()
 
-frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.8, relheight=0.3, relx = 0.1, rely = 0.1)
 
-frame2 = tk.Frame(root, bg="white")
-frame2.place(relwidth=0.8, relheight=0.3, relx = 0.1, rely = 0.5)
 
 
 
@@ -126,10 +164,11 @@ def summarizeTexts(text):
     for widget in frame2.winfo_children():
         widget.destroy()
     text = translator.translate(text, dest='en').text
+    textr = translator.translate(text, dest='tr').text
+    #print("SUMMARIZED", textr)
     summary_text = summarizer(text, max_length=120000000000, min_length=5, do_sample=False)[0]['summary_text']
     summary_text = translator.translate(summary_text, dest='tr').text
-    label = tk.Label(frame2, text=summary_text, wraplength=500)
-    label.pack()
+    return summary_text
 
 
 
@@ -145,22 +184,54 @@ def summarizeFromPDF():
         label = tk.Label(frame, text=app)
         label.pack()
     str1 = PDF_read.decode('UTF-8')
-    print("LANOFLAN", translator.detect(str1).lang)
     if (translator.detect(str1).lang == "en"): 
-        str1 = makeMeaningful(str1)
-    label = tk.Label(frame, text=str1, wraplength=500)
-    label.pack()  
-    summarizeTexts(str1)
+            str1 = makeMeaningful(str1)
+    #print("DIVIDED", str1[0:25])
+    #print("LENOFSTRINFG", len(str1))
+    #count = int(len(str1) / 300)
+    #print("COUTN", count)
+    summarized = ""
+    i = 0 
+    end = 70
+    slice = str1.split()
+    #print("TYPE", type(len(slice)))
+    lenofwords = len(slice)
+
+    count = int(lenofwords/70)
+    mod = lenofwords % 70
+    for x in range(0, count):
+        temp = slice[i:end+mod]
+        tempText = ""
+        control = ""
+        #print("TEMP", temp)
+        for x in range(0, len(temp)):
+            control += temp[x] + " "
+            #print("SLICED", temp[x])
+            tempText += temp[x] + " "
+            x+=1
+        #print("TEMPED", tempText)
+        print("CONTROL", control)
+        summarized = summarized + summarizeTexts(tempText)
+        print("SUMMARYCHECK", summarizeTexts(tempText))
+        i += 70
+        end += 70
+        x += 1
+    #print("FINAL SUMMARIZATION")
+    #print("LANOFLAN", translator.detect(str1).lang)
+    label = tk.Label(frame, text=str1, wraplength=800,  fg="white", bg= "#52adc8")
+    label.pack()
+    label = tk.Label(frame2, text=summarized, wraplength=800,  fg="white", bg= "#52adc8")
+    label.pack()
 
 def summarizeFromAudio():
         for widget in frame.winfo_children():
             widget.destroy()
         filename = filedialog.askopenfilename(initialdir="/", title="Select File", 
         filetypes=(("WAV", "*.wav"), ("all files", "*.wav*")))
-        print("FILENAME IS THAT", filename)
+        #print("FILENAME IS THAT", filename)
         Audio_read = get_large_audio_transcription(filename)
-        print("AUDIO", Audio_read)
-        print("TYPEOF", type(Audio_read))
+        #print("AUDIO", Audio_read)
+        #print("TYPEOF", type(Audio_read))
         for app in apps: 
             label = tk.Label(frame, text=app)
             label.pack()
@@ -168,10 +239,12 @@ def summarizeFromAudio():
         label.pack()  
         summarizeTexts(Audio_read)
 
-summarizeTextFromPDF = tk.Button(root, text="Summarize Text From DOCX", padx=10, pady=5, fg="white", bg="#263D42", command=summarizeFromPDF)
-summarizeTextFromPDF.pack()
 
-summarizeTextFromAudio = tk.Button(root, text="Summarize Text From Audio File", padx=10, pady=5, fg="white", bg="#263D42", command=summarizeFromAudio)
-summarizeTextFromAudio.pack()
+copyText_btn = Button(root, text="Summarize From DOCX", font=("shanti", 10), height=1, width=20, command=summarizeFromPDF, bg="#55c39e", fg="white")
+saveAll_btn = Button(root, text="Summarize From Audio", font=("shanti", 10), height=1, width=20, command = summarizeFromAudio, bg="#55c39e", fg="white")
+#place buttons on grid
+copyText_btn.grid(row=3,column=0)
+saveAll_btn.grid(row=3,column=2)
+
 
 root.mainloop()
